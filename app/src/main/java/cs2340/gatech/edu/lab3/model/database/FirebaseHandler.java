544034477200b43fd3b9da5362f95fcec9075aed
DESKTOP-2NIVFK9\rachel <tierne1t@gmail.com>
2018-09-14 10:47:04 -0400

    Added Firebase to gradle files and FirebaseHandler class

---
+++ app/src/main/java/cs2340/gatech/edu/lab3/model/database/FirebaseHandler.java
@@ -0,0 +1,40 @@
+package cs2340.gatech.edu.lab3.model.database;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirebaseHandler {
+    public static void createSampleUser(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("FirebaseHandler", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("FirebaseHandler", "Error adding document", e);
+                    }
+                });
+    }
+
+}
